b29bd78604228d0d7103286885077c8a224f5e26
Jeff Duda <jeff.duda@gmail.com>
2014-10-07 22:57:48 -0400

    ADD: check for inputs

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-curl -o ../data/input/PEDS012_20131101.zip http://files.figshare.com/1701182/PEDS012_20131101.zip
-unzip -d ../data/input/ ../data/input/PEDS012_20131101.zip
+if [ ! -d ../data/input/PEDS012 ]; then
+  curl -o ../data/input/PEDS012_20131101.zip http://files.figshare.com/1701182/PEDS012_20131101.zip
+  unzip -d ../data/input/ ../data/input/PEDS012_20131101.zip
+fi
 
-curl -o ../data/template/  http://files.figshare.com/1510696/PTBP.zip
-unzip -d ../data/template/ ../data/template/PTBP.zip
+if [ ! -d ../data/templates/PTBP ]; then
+  curl -o ../data/template/PTBP.zip  http://files.figshare.com/1510696/PTBP.zip
+  unzip -d ../data/template/ ../data/template/PTBP.zip
+fi
 
 ./process_t1.sh
 ./process_modalities.sh
