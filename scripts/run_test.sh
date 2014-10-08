bd2443e35e51449758cacb38d3593b4e2f8583b6
Jeff Duda <jeff.duda@gmail.com>
2014-10-07 22:55:11 -0400

    ADD: download template

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,6 +2,10 @@
 
 curl -o ../data/input/PEDS012_20131101.zip http://files.figshare.com/1701182/PEDS012_20131101.zip
 unzip -d ../data/input/ ../data/input/PEDS012_20131101.zip
+
+curl -o ../data/template/  http://files.figshare.com/1510696/PTBP.zip
+unzip -d ../data/template/ ../data/template/PTBP.zip
+
 ./process_t1.sh
 ./process_modalities.sh
 ./run_comparison.R PEDS012 20131101
