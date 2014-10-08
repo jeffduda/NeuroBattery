0774dc2fcb08062a0393bedc5717a11f04153139
Jeff Duda <jeff.duda@gmail.com>
2014-10-07 22:47:51 -0400

    ENH: Download input data

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+curl -o ../data/input/PEDS012_20131101.zip http://files.figshare.com/1701182/PEDS012_20131101.zip
+unzip -d ../data/input/ ../data/input/PEDS012_20131101.zip
 ./process_t1.sh
 ./process_modalities.sh
 ./run_comparison.R PEDS012 20131101
