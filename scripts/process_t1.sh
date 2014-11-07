54c4f83d8e1856869906b177d3fead2c5ddb01fd
BBAvants <stnava@gmail.com>
2014-11-07 11:19:50 -0500

    BUG: mask names

--- scripts/process_t1.sh
+++ scripts/process_t1.sh
@@ -3,7 +3,7 @@ sh  ${ANTSPATH}/antsCorticalThickness.sh -d 3 \
    -a ../data/input/PEDS012/20131101/Anatomy/PEDS012_20131101_mprage_t1.nii.gz \
    -e ../data/template/PTBP/PTBP_T1_Defaced.nii.gz \
    -m ../data/template/PTBP/PTBP_T1_BrainCerebellumProbabilityMask.nii.gz \
-   -f ../data/template/PTBP/PTBP_T1_ExtractionMask \
+   -f ../data/template/PTBP/PTBP_T1_ExtractionMask.nii.gz \
    -p ../data/template/PTBP/Priors/priors%d.nii.gz \
    -t ../data/template/PTBP/PTBP_T1_BrainCerebellum.nii.gz \
    -k 1 \
