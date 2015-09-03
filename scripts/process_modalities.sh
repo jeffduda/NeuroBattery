afd66001feed732f6aef52868b9e6db9e472535a
Jeffrey Duda <jeff.duda@gmail.com>
2015-09-03 12:27:23 -0400

    FIX: fix dwi filenames

--- scripts/process_modalities.sh
+++ scripts/process_modalities.sh
@@ -1,7 +1,7 @@
 # Convert DWI to DTI
 
 if [ ! -d "../data/input/DTI" ]; then
-  sh ${PIPEDREAMPATH}/nii2dt/nii2dt.sh --dwi ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.nii.gz   ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.nii.gz --bvals ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.bval ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0019_DTI_1_1x0_30x1000.bval --bvecs ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.bvec ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0019_DTI_1_1x0_30x1000.bvec --outroot PEDS012_20131101_DTI_ --outdir ../data/input/PEDS012/20131101/DTI
+  sh ${PIPEDREAMPATH}/nii2dt/nii2dt.sh --dwi ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.nii.gz   ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.nii.gz --bvals ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0019_DTI_1_1x0_30x1000.bval ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0019_DTI_1_1x0_30x1000.bval --bvecs ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0013_DTI_1_1x0_30x1000.bvec ../data/input/PEDS012/20131101/DWI/PEDS012_20131101_0019_DTI_1_1x0_30x1000.bvec --outroot PEDS012_20131101_DTI_ --outdir ../data/input/PEDS012/20131101/DTI
 else
   echo "DTI has already been reconstructed, skipping this step. Delete NeuroBattary/data/input/PEDS012/2013110/DTI to rerun the reconstruction"
 fi
