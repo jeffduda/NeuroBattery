6e22ae82e29140c559e13391b21ca8e0797daa57
Jeffrey Duda <jeff.duda@gmail.com>
2017-09-12 12:45:16 -0400

    FIX: fix PCASL processing

--- scripts/warp_labels.sh
+++ scripts/warp_labels.sh
@@ -12,15 +12,11 @@ ${ANTSPATH}/antsApplyTransforms -d 3 \
 -r ${SUBJECTT1DIR}${SUBJECTID}_BrainExtractionMask.nii.gz \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject1GenericAffine.mat \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject0Warp.nii.gz \
--o ${SUBJECTT1DIR}${SUBJECTID}_aal.nii.gz -n MultiLabel
+-o ${SUBJECTT1DIR}${SUBJECTID}_aal.nii.gz -n MultiLabel -v 1
 
 ${ANTSPATH}/ImageMath 3 ${SUBJECTT1DIR}${SUBJECTID}_aal.nii.gz m \
 ${SUBJECTT1DIR}${SUBJECTID}_aal.nii.gz \
 ${SUBJECTT1DIR}${SUBJECTID}_BrainExtractionMask.nii.gz
-
-${ANTSPATH}/ImageMath 3 ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz m \
-${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz \
-${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_brainmask.nii.gz
 
 ${ANTSPATH}/antsApplyTransforms -d 3 \
 -i ${TEMPLATEDIR}/PTBP/Labels/PTBP_T1_AAL.nii.gz \
@@ -29,34 +25,36 @@ ${ANTSPATH}/antsApplyTransforms -d 3 \
 -t ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_1InverseWarp.nii.gz \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject1GenericAffine.mat \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject0Warp.nii.gz \
--o ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz -n MultiLabel
+-o ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz -n MultiLabel -v 1
 
 ${ANTSPATH}/ImageMath 3 ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz m \
 ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_aal.nii.gz \
 ${SUBJECTBOLDDIR}${SUBJECTID}_BOLD_brainmask.nii.gz
 
-antsApplyTransforms -d 3 \
+${ANTSPATH}/antsApplyTransforms -d 3 \
 -i ${TEMPLATEDIR}/PTBP/Labels/PTBP_T1_AAL.nii.gz \
 -r ${SUBJECTDTIDIR}${SUBJECTID}_DTI_brainmask.nii.gz \
 -t [ ${SUBJECTDTIDIR}${SUBJECTID}_DTI_0GenericAffine.mat,1 ] \
 -t ${SUBJECTDTIDIR}${SUBJECTID}_DTI_1InverseWarp.nii.gz \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject1GenericAffine.mat \
 -t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject0Warp.nii.gz \
--o ${SUBJECTDTIDIR}${SUBJECTID}_DTI_aal.nii.gz -n MultiLabel
+-o ${SUBJECTDTIDIR}${SUBJECTID}_DTI_aal.nii.gz -n MultiLabel -v 1
 
 ${ANTSPATH}/ImageMath 3 ${SUBJECTDTIDIR}${SUBJECTID}_DTI_aal.nii.gz m \
 ${SUBJECTDTIDIR}${SUBJECTID}_DTI_aal.nii.gz \
 ${SUBJECTDTIDIR}${SUBJECTID}_DTI_brainmask.nii.gz
 
-antsApplyTransforms -d 3 \
--i ${TEMPLATEDIR}/PTBP/Labels/PTBP_T1_AAL.nii.gz \
--r ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_brainmask.nii.gz \
--t [ ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_0GenericAffine.mat,1] \
--t ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_1InverseWarp.nii.gz \
--t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject1GenericAffine.mat \
--t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject0Warp.nii.gz \
--o ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz -n MultiLabel
+${ANTSPATH}/ThresholdImage 3 ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_SegmentationWarpedToASL.nii.gz ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_brainmask.nii.gz 1 inf
 
-${ANTSPATH}/ImageMath 3 ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz m \
-${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz \
-${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_brainmask.nii.gz
+#${ANTSPATH}/antsApplyTransforms -d 3 \
+#-i ${TEMPLATEDIR}/PTBP/Labels/PTBP_T1_AAL.nii.gz \
+#-r ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_brainmask.nii.gz \
+#-t [ ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_0GenericAffine.mat,1] \
+#-t ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_1InverseWarp.nii.gz \
+#-t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject1GenericAffine.mat \
+#-t ${SUBJECTT1DIR}${SUBJECTID}_TemplateToSubject0Warp.nii.gz \
+#-o ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz -n MultiLabel -v 1
+
+#${ANTSPATH}/ImageMath 3 ${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz m \
+#${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_aal.nii.gz \
+#${SUBJECTPCASLDIR}${SUBJECTID}_PCASL_brainmask.nii.gz
